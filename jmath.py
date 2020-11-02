5069ce83ea395f011fe1cd8a1646f9fe429756d9
Jordan Hay <jordanhay0112@gmail.com>
2020-11-02 18:52:33 +1300

    Init commit

---
+++ jmath.py
@@ -0,0 +1,18 @@
+'''
+    jmath.py
+
+    Author: Jordan Hay
+    Date: 2020-11-02
+
+    Jordan's Math Module
+
+    Creating my own maths tools in Python as a challenge.
+'''
+
+# - Classes
+
+# -- nVector
+#
+class nVector:
+
+    pass
