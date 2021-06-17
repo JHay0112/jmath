17f1529077fa33cfdfef001f1eb6f75b97834df7
Jordan Hay <jordanhay0112@gmail.com>
2021-06-17 12:26:01 +1200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+'''
+    setup.py
+
+    Author: Jordan Hay
+    Date: 2021-06-17
+
+    
+'''
+
+from setuptools import find_packages, setup
+
+setup(
+    name = 'jmath',
+    packages = find_packages(include=['jmath']),
+    version = '1.0',
+    description = "Mathematics Tools",
+    author = "Jordan Hay",
+    license = "None",
+    install_requires=["matplotlib==3.3.4"]
+)
