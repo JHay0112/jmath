e908896a56ce0ae878271899b2e2bbb891ef77a9
Jordan Hay <jordanhay0112@gmail.com>
2021-06-22 11:42:08 +1200

    Basic circuit components

--- jmath/physics/circuits.py
+++ jmath/physics/circuits.py
@@ -14,7 +14,64 @@ from ..discrete import *
 # - Classes
 
 class Circuit(Graph):
-    pass
+    
+    def add_components(self, *components):
+        """
+            Add components to circuit
+
+            *components (Components) - Components to add to circuit
+        """
+        super().add_nodes(*components)
 
 class Component(Node):
-    pass
+    
+    def __init__(self, id, weight = 0):
+        """
+            Base for circuit components
+
+            id (str) - ID of component e.g. R1
+            weight (float:0) - Component weighting (resistance for resistors etc.)
+
+            Author: Jordan Hay
+            Date: 2021-06-22
+        """
+
+        super().__init__(id, weight)
+
+    def connect(self, neighbour):
+        """
+            Adds a neighbouring component
+
+            neighbour (Component) - Neighbouring component to add
+        """
+        super().add_neighbour(neighbour, 0, True)
+
+class Resistor(Component):
+
+    def __init__(self, id, resistance):
+        """
+            Ideal Resistor
+
+            id (str) - ID of component
+            resistance (float) - Resistance in Ohms of the resistor
+
+            Author: Jordan Hay
+            Date: 2021-06-22
+        """
+        self.resistance = resistance
+        super().__init__(id, resistance)
+
+class DCVoltageSource(Component):
+
+    def __init__(self, id, voltage):
+        """
+            Supplies a DC voltage to a circuit
+
+            id (str) - ID of component
+            voltage (float) - Voltage in Volts to supply
+
+            Author: Jordan Hay
+            Date: 2021-06-22
+        """
+        self.voltage = voltage
+        super().__init__(id, voltage)
