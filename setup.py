cba443a83cb8787f54840b20b907520223059f37
Jordan Hay <jordanhay0112@gmail.com>
2021-06-21 22:35:18 +1200

    Rename as 1.2.0, 2.0.0 reserved for docstr rewrite

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 setup(
     name = 'jmath',
     packages = find_packages(include=['jmath']),
-    version = '2.0.0',
+    version = '1.2.0',
     description = "Mathematics Tools",
     author = "Jordan Hay",
     license = "MIT",
