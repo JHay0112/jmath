75a5bba140f00f23b577c6f45366fec86a788b8a
Jordan Hay <jordanhay0112@gmail.com>
2021-06-22 10:58:05 +1200

    Update docstrings of linearalgebra and uncertainty

--- jmath/linearalgebra.py
+++ jmath/linearalgebra.py
@@ -14,80 +14,64 @@ import operator # Python operators
 import math # Mathematical functions, e.g. sqrt()
 
 # - Classes
-
-# -- Vector
-# Author: Jordan Hay
-# Date: 2020-11-02
-# A vector with n dimensions
 class Vector:
 
-    # --- __init__()
-    # Initialise Vector object
-    #
-    # self
-    # *components (List/*args) - Scalar vector components, e.g. x, y, z
     def __init__(self, *components):
+        """
+            n-dimensional Vectors
+
+            *components (floats) - Scalar vector components
+
+            Author: Jordan Hay
+            Date: 2020-11-02
+        """
 
         # If components[0] is list store that list
         if(type(components[0]) == list):
-            self._components = components[0]
+            self.components = components[0]
         else:
             # Else it's *args
-            self._components = components
+            self.components = components
 
-    # --- __add__()
-    # Add another Vector object to this Vector
-    #
-    # self
-    # vector (Vector) - The foreign Vector to add
     def __add__(self, vector):
+        """
+            Add vectors together
+
+            vector (Vector) - Vector to add
+        """
 
         # Add the foreign components to local components and return
-        return(Vector(list(map(operator.add, self._components, vector.components()))))
+        return(Vector(list(map(operator.add, self.components, vector.components()))))
 
-    # --- __sub__()
-    # Subtract another Vector object from this vector
-    #
-    # self
-    # vector (Vector) - The foreign Vector to subtract
     def __sub__(self, vector):
+        """
+            Subtract vectors from each other
 
+            vector (Vector) - Vector to subtract
+        """
         # Subtract the foreign components from local components and return
-        return(Vector(list(map(operator.sub, self._components, vector.components()))))
+        return(Vector(list(map(operator.sub, self.components, vector.components()))))
 
-    # --- components()
-    # Returns the components of the vector
-    #
-    # self
-    def components(self):
-
-        return(self._components)
-
-    # --- magnitude()
-    # Returns the magnitude of the vector as calculated from the components
-    #
-    # self
     def magnitude(self):
-
+        """Calculates the vector magnitude"""
         # Store magnitude while computing
         magnitude = 0
 
         # For every vector component
-        for c in self._components:
+        for c in self.components:
             # Pythagorean theorom
             # hypotenuse = sqrt(a**2 + b**2)
             magnitude = math.sqrt(magnitude ** 2 + c ** 2)
 
         return(magnitude)
-
-    # --- draw()
-    # Draws a 2D arrow
-    #
-    # self
-    # x (int) - The dimension to plot on the x-axis
-    # y (int) - The dimension to plot on the y-axis
+        
     def draw(self, x = 0, y = 1):
+        """
+            Draws a 2D vector with matplot lib
 
+            x (int) - Index of component to plot on the x-axis
+            y (int) - Index of component to plot on the y-axis
+        """
         ax = plt.axes()
-        ax.arrow(0, 0, self._components[x], self._components[y], head_width=0.05, head_length=0.05)
+        ax.arrow(0, 0, self.components[x], self.components[y], head_width=0.05, head_length=0.05)
         plt.show()
