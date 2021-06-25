5518b601efeb98d8b55a454a5d0cf6a7ad92e256
Jordan Hay <jordanhay0112@gmail.com>
2021-06-25 23:39:21 +1200

    Fix testing method for vector addition

--- jmath/tests/test_linearalgebra.py
+++ jmath/tests/test_linearalgebra.py
@@ -1,15 +1,20 @@
 """
     jmath/tests/test_linearalgebra.py
 
-    Runs tests on linear algebra component
+    Runs tests on linear algebra component using pytest
 
     Author: Jordan Hay
     Date: 2021-06-25
 """
 
+# - Imports
+
 from ..linearalgebra import *
 
+# - Functions
+
 def test_vector_addition():
+    """Tests vector addition"""
     added = Vector(1, 2) + Vector(3, 4)
     excepted = Vector(4, 6)
-    assert added == excepted
+    assert added.components == excepted.components
