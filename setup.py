432a346688f93c4581aa80e682945df4ee6ad8be
Jordan Hay <jordanhay0112@gmail.com>
2021-07-07 17:24:08 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 setup(
     name = 'jmath',
     packages = find_packages(include=['jmath']),
-    version = 'v3.0.0',
+    version = 'v3.0.1',
     description = "Mathematics Tools",
     author = "Jordan Hay",
     license = "MIT",
