788c52d0828d35e1491074e3720f904628a1b2ec
Jordan Hay <jordanhay0112@gmail.com>
2021-07-16 14:42:52 +1200

    Test new vector funcs and gravity

---
+++ tests/test_mechanics.py
@@ -0,0 +1,25 @@
+"""
+    jmath/tests/test_mechanics.py
+
+    Runs tests on Mechanics 
+
+    Author: Jordan Hay
+    Date: 2021-07-16
+"""
+
+# - Imports
+
+from ..jmath.physics.mechanics import *
+
+# - Tests
+
+def test_gravity():
+    """Tests that the graviational force between two objects at a distance is as expected."""
+    env = PhysEnv()
+    obj1 = PhysObj(env, Point(0, 0), Vector(0, 0), 1)
+    obj2 = PhysObj(env, Point(1, 0), Vector(0, 0), 2)
+    expected_grav = (GRAVITATIONAL_CONSTANT * 1 * 2)
+    # Pointing from obj1 to obj2
+    expected_grav_vec = Vector(expected_grav, 0)
+    assert obj1.gravity(obj2).components == expected_grav_vec.components
+    assert obj2.gravity(obj1).components == expected_grav_vec.negative().components
