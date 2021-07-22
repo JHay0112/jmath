daeae6664f068a5559fe5cf6897d3fc8342d36a6
Jordan Hay <jordanhay0112@gmail.com>
2021-07-22 21:47:33 +1200

    Add back zerodistance error

--- jmath/exceptions.py
+++ jmath/exceptions.py
@@ -19,6 +19,12 @@ class VectorsNotSameSize(Exception):
         self.message = message
         super().__init__(self.message)
 
+class ZeroDistance(Exception):
+    """Exception thrown for calculations with zero distance between objects."""
+    def __init__(self, message = "Invalid operation! Zero distance between objects."):
+        self.message = message
+        super().__init__(self.message)
+
 class InvalidFractionOperation(Exception):
     """Exception thrown for invalid operations on fractions"""
     def __init__(self, message = "This operation cannot be completed on fractions."):
