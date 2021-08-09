134536bfcf097c5206fb2baf0daab1181566f978
Jordan Hay <jordanhay0112@gmail.com>
2021-08-09 20:52:18 +1200

    Queues and queue tests

--- jmath/abstract/queues.py
+++ jmath/abstract/queues.py
@@ -4,3 +4,27 @@
 
 # - Imports
 
+from .lists import LinkedList
+from typing import Any
+
+# - Classes
+
+class Queue(LinkedList):
+    """
+        Queue Abstract Data Type
+    """
+
+    def enqueue(self, data: Any):
+        """
+            Enqueues a piece of data onto the queue.
+
+            data
+                The piece of data to enqueue.
+        """
+        super().append(data)
+
+    def dequeue(self):
+        """Returns and removes the first queue item."""
+        head = self.head
+        self.head = head.next
+        return head.data
---
+++ tests/test_queues.py
@@ -0,0 +1,36 @@
+"""
+    Tests Queue Abstract Data Types
+"""
+
+# - Imports
+
+from typing import Deque
+from ..jmath.abstract.queues import Queue
+
+# - Tests
+
+def test_enqueue_dequeue():
+    """Tests queues and dequeueing."""
+    data = [3, 2, 1]
+    q = Queue()
+    
+    # Enqueue
+    for item in data:
+        q.enqueue(item)
+
+    # Dequeue
+    for i in range(len(data)):
+        assert data[i] == q.dequeue()
+
+def test_listify():
+    """Tests that a queue list is generated as expected."""
+    data = [3, 2, 1]
+    q = Queue()
+    
+    # Enqueue
+    for item in data:
+        q.enqueue(item)
+
+    lst = q.list()
+
+    assert lst == data
