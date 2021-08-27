a91a678c7bfe4ca938977424b2abc55d65999d91
Jordan Hay <jordanhay0112@gmail.com>
2021-08-27 22:12:37 +1200

    Update init file

--- jmath/linearalgebra/__init__.py
+++ jmath/linearalgebra/__init__.py
@@ -13,3 +13,7 @@ __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 # - Defaults
 
+from .vectors import Vector
+from .points import Point, Complex
+from .lines import Line
+from .planes import Plane
