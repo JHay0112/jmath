71bed25175371a3f49b4409291c2419e7ef82f8d
Jordan Hay <jordanhay0112@gmail.com>
2021-08-27 22:01:37 +1200

    Setup new components structure

---
+++ jmath/linearalgebra/__init__.py
@@ -0,0 +1,15 @@
+'''
+    Linear Algebra Tools
+
+    Default Packages
+    ----------------
+
+    
+'''
+
+# - Namespace
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+# - Defaults
+
