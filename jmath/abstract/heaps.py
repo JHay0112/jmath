83d67091c369d7aedee2436c3c9be5e2e63199a6
Jordan Hay <jordanhay0112@gmail.com>
2021-10-12 19:57:01 +1300

    Generic sift up, down algorithms

--- jmath/abstract/heaps.py
+++ jmath/abstract/heaps.py
@@ -11,12 +11,22 @@ from typing import Optional, Union, List
 class BinaryHeap:
     '''
         Implementation of a Generic Binary Heap
+
+        Attributes
+        ----------
+
+        compare : Callable
+            Defines the comparison operator for the heap, should take a set of values and return one per an ordering. 
+            For example the comparison operator for a min heap is the min function.
     '''
+
+    compare = None
+
     def __init__(self):
 
         self.items = []
 
-    def parent(self, index: int) -> Optional[int]:
+    def parent_of(self, index: int) -> Optional[int]:
         '''
             Calculates the location of the parent of a node
 
@@ -28,7 +38,7 @@ class BinaryHeap:
 
             Returns
             -------
-
+        
             The index of the parent node or None if it does not have a parent
         '''
 
@@ -38,7 +48,7 @@ class BinaryHeap:
             # Calculate and return parent index
             return (index - 1)//2
 
-    def children(self, index: int) -> Optional[Union[List[int, int], List[int]]]:
+    def children_of(self, index: int) -> Optional[Union[List[int, int], List[int]]]:
         '''
             Calculates the locations of the children of a node
 
@@ -68,3 +78,66 @@ class BinaryHeap:
 
         # Return as tuple
         return children
+
+    def sift_down(self, index: int):
+        """
+            Performs a sift down operation on a given index
+
+            Parameters
+            ----------
+
+            index
+                The index to perform the sift down operation on
+        """
+
+        # While there are children
+        while children := self.children_of(index) is not None:
+            # For two children
+            if len(children) == 2:
+                # Get child values
+                child_vals = [self.items[i] for i in children]
+                # Compare values
+                child_val = self.compare(child_vals)
+                # If child 1 matches
+                if self.items[children[0]] == child_val:
+                    child = children[0]
+                else:
+                    child = children[1]
+            else:
+                # For one child
+                child = children[0]
+            # Compare child to parent
+            best_val = self.compare(self.items[child], self.items[index])
+            # If the child is the in order value
+            if best_val == self.items[child]:
+                # Swap parent and item
+                self.items[index], self.items[child] == self.items[child], self.items[index]
+                # Mark child index as new index to look at 
+                index = child
+            else:
+                # Else there is no swap to do, break
+                break
+
+    def sift_up(self, index: int):
+        """
+            Performs a sift up operation on a given index
+
+            Parameters
+            ----------
+
+            index
+                The index to perform the sift up operation on
+        """
+
+        while parent := self.parent_of(index) is not None:
+            # Compare child and parent
+            best_val = self.compare(self.items[index], self.items[parent])
+            # If the parent is the better value
+            if best_val == self.items[parent]:
+                # Swap parent and item
+                self.items[index], self.items[parent] == self.items[parent], self.items[index]
+                # Mark parent as next to look at
+                index = parent
+            else:
+                # There is no swap to do, break
+                break
