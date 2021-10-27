01269d5262663ddb11eb67e70ebbb71f11976a15
Jordan Hay <jordanhay0112@gmail.com>
2021-10-28 10:28:54 +1300

    More primality checks

--- jmath/loud/primes.py
+++ jmath/loud/primes.py
@@ -5,10 +5,11 @@
 # - Imports
 
 from math import sqrt, ceil
+from .modular import gcd
 
 # - Functions
 
-def primality_check(n: int):
+def primality(n: int):
     '''
         Check that a number is prime.
 
@@ -27,9 +28,30 @@ def primality_check(n: int):
 
         if n % i == 0:
             print(f"\n{i}|{n} implies {n} is not prime")
-            break
+            return False
         else:
             print(f"{i} does not divide {n}")
 
     else:
         print(f"\nThus, {n} is prime")
+        return True
+
+def relative_primality(a: int, b: int):
+    '''
+        Check that a number is relatively prime to another number
+
+        Parameters
+        ----------
+
+        a
+            The number to check the relative primality of
+        b
+            The number to check the relative primality with respect to
+    '''
+
+    if gcd(a, b) == 1:
+        print(f"Thus {a} is relatively prime with respect to {b}.")
+        return True
+    else:
+        print(f"Thus {a} is NOT relatively prime with respect to {b}.")
+        return False
