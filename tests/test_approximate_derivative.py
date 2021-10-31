e4a892e4ba8ac23357c951a59b4b8b188b96befb
Jordan Hay <jordanhay0112@gmail.com>
2021-11-01 12:16:17 +1300

    SPT test with paraboloid

--- tests/test_approximate_derivative.py
+++ tests/test_approximate_derivative.py
@@ -4,8 +4,8 @@
 
 # - Imports
 
-from ..jmath.approximation.differentiation import differentiate
-from .tools import random_integer, repeat
+from ..jmath.approximation.differentiation import differentiate, second_partials_test, LOCAL_MAXIMUM, LOCAL_MINIMUM, SADDLE
+from .tools import plus_or_minus, random_integer, repeat
 from math import sin, cos, pi
 
 # - Tests
@@ -37,3 +37,26 @@ def test_second_linear_derivative():
     point = random_integer()
     f = lambda x : gradient * x + y_int
     assert round(differentiate(f, point, n = 2).value, 5) == 0
+
+@repeat
+def paraboloid_second_partials():
+    """Tests the second partials test on a paraboloid"""
+    a = plus_or_minus()
+    b = plus_or_minus()
+    c = random_integer()
+    # Construct ellipse
+    paraboloid = lambda x, y: a*x**2 + b*y**2 + c
+    # Get critical point class at (0, 0)
+    type = second_partials_test(paraboloid, [(0, 0)])[(0, 0)]
+    # Check if correct type
+    if a == b:
+        # Both negative
+        if a == -1:
+            # Thus max
+            assert type == LOCAL_MAXIMUM
+        else:
+            # Else both positive so min
+            assert type == LOCAL_MINIMUM
+    else:
+        # Different co-effecients, should be saddle
+        assert type == SADDLE
