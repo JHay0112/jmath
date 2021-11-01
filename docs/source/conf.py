bc58e0c676a15987a6a7dfa742028be7ceffbc7f
Jordan Hay <jordanhay0112@gmail.com>
2021-11-01 13:45:55 +1300

    Add base url

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,6 +50,9 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
+# Domain name
+html_base_url = 'jmath.jordanhay.com'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
