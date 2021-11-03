b4c5b28a3e0f21414658b3879a067bfff92ce75d
Jordan Hay <jordanhay0112@gmail.com>
2021-11-04 08:41:29 +1300

    Messy prufer solver

--- jmath/loud/__init__.py
+++ jmath/loud/__init__.py
@@ -10,3 +10,4 @@ __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 from .modular import gcd, modular_inverse_brute_force
 from .primes import primality, relative_primality
+from .graphs import prufer_solver
