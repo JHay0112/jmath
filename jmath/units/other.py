138268893101de77460aef8c223c3da955c0d283
Jordan Hay <jordanhay0112@gmail.com>
2021-11-06 13:47:26 +1300

    Particle physics style units

--- jmath/units/other.py
+++ jmath/units/other.py
@@ -6,7 +6,7 @@
 
 from .units import Unit
 from .conversion import define_conversion, define_alias
-from .si import kelvin
+from .si import joule, kelvin, metre, second, kilogram, c, e
 from math import pi
 
 # - Globals
@@ -20,3 +20,16 @@ define_conversion(radian, degree, 180/pi)
 centigrade = Unit("°C")
 define_conversion(centigrade, kelvin, lambda x: x + 273.15)
 define_conversion(kelvin, centigrade, lambda x: x - 273.15)
+
+# Physics
+# -- Light Speed
+lightspeed = Unit("c")
+define_conversion(lightspeed, metre/(second ** 2), c)
+# -- Electron Volts
+electron_volt = Unit("eV")
+define_conversion(electron_volt, joule, e)
+# -- Electron Volt Mass Energy
+electron_volt_mass = electron_volt/(lightspeed**2)
+define_conversion(electron_volt_mass, electron_volt, 1)
+define_conversion(electron_volt_mass, kilogram, e/(c**2))
+define_conversion(electron_volt_mass, joule, e)
