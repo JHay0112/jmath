22b856000e0231d9962c8d392e3bc5aa74e33624
Jordan Hay <jordanhay0112@gmail.com>
2021-11-08 18:57:50 +1300

    Create universal package

---
+++ jmath/universal/__init__.py
@@ -0,0 +1,9 @@
+'''
+    Standard maths functions that are also compatible with jmath structures.
+'''
+
+# - Namespace
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+# - Defaults
