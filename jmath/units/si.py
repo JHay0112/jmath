d728d100795af72fb085df1d717004232a5ce769
Jordan Hay <jordanhay0112@gmail.com>
2021-11-08 22:08:38 +1300

    SI UnitSpace

--- jmath/units/si.py
+++ jmath/units/si.py
@@ -15,65 +15,94 @@
 # - Imports
 
 from .units import Unit
-from .conversion import define_alias, define_conversion
+from .conversion import UnitSpace
 
 # - Main
 
-# Base Units
+# Unit space
 
-metre = Unit("m")
-second = Unit("s")
-kilogram = Unit("kg")
-ampere = Unit("A")
-kelvin = Unit("K")
-mole = Unit("mol")
-candela = Unit("cd")
+si = UnitSpace()
 
-# Derived Units
+# Prefixes
 
-newton = Unit("N")
-define_alias(kilogram * metre / (second ** 2), newton)
+prefixes = {
+    "Y": 1e24,
+    "Z": 1e21,
+    "E": 1e18,
+    "P": 1e15,
+    "T": 1e12,
+    "G": 1e9,
+    "M": 1e6,
+    "k": 1e3,
+    "m": 1e-3,
+    "μ": 1e-6,
+    "n": 1e-9,
+    "p": 1e-12,
+    "f": 1e-15,
+    "a": 1e-18,
+    "z": 1e-21,
+    "y": 1e-24
+}
 
-joule = Unit("J")
+# Units
 
-coulomb = Unit("C")
-define_alias(ampere * second, coulomb)
+unit_names = [
+    "m", "s", "g", "A", "K",
+    "mol", "cd", "N", "J", "C",
+    "Pa", "W", "V", "F", "Ω",
+    "S", "Wb", "T", "H"
+]
 
-pascal = Unit("Pa")
-define_alias(newton/(metre ** 2), pascal)
+units = {unit: Unit(unit, si) for unit in unit_names}
 
-watt = Unit("W")
-define_alias(joule/second, watt)
+# Conversion
+for prefix, factor in prefixes.items():
+    for name, unit in units.items():
+        combo = prefix + name
+        si.define_conversion(Unit(combo, si), unit, factor)
 
-volt = Unit("V")
-define_alias(watt/ampere, volt)
+# Names
 
-farad = Unit("F")
-define_alias(coulomb/volt, farad)
-
-ohm = Unit("Ω")
-define_alias(ampere * ohm, volt)
-define_alias(volt/ampere, ohm)
-
-siemens = Unit("S")
-define_alias(ampere/volt, siemens)
-
-weber = Unit("Wb")
-define_alias(volt * second, weber)
-
-tesla = Unit("T")
-define_alias(weber/(metre**2), tesla)
-
-henry = Unit("H")
-define_alias(weber/ampere, henry)
+metre = units["m"]
+second = units["s"]
+kilogram = Unit("kg", si)
+ampere = units["A"]
+kelvin = units["K"]
+mole = units["mol"]
+candela = units["cd"]
+newton = units["N"]
+joule = units["J"]
+coulomb = units["C"]
+pascal = units["Pa"]
+watt = units["W"]
+volt = units["V"]
+farad = units["F"]
+ohm = units["Ω"]
+siemens = units["S"]
+weber = units["Wb"]
+tesla = units["T"]
+henry = units["H"]
 
 # Standard Forms
 
 displacement = metre
-
 velocity = metre/second
-
 acceleration = metre/(second**2)
+
+# Aliases
+
+si.define_alias(kilogram * metre / (second ** 2), newton)
+si.define_alias(ampere * second, coulomb)
+si.define_alias(newton/(metre ** 2), pascal)
+si.define_alias(joule/second, watt)
+si.define_alias(watt/ampere, volt)
+si.define_alias(coulomb/volt, farad)
+si.define_alias(ampere * ohm, volt)
+si.define_alias(volt/ampere, ohm)
+si.define_alias(ampere/volt, siemens)
+si.define_alias(volt * second, weber)
+si.define_alias(weber/(metre**2), tesla)
+si.define_alias(weber/ampere, henry)
 
 # Base Constants
 
@@ -83,7 +112,6 @@ e = 1.602e-19 * coulomb
 k = 1.381e-23 * joule/kelvin
 N = 6.022e23 / mole
 
-# Unit conversions
+# Conversions
 
-# Mass to energy
-define_conversion(kilogram, joule, c**2)
+si.define_conversion(kilogram, joule, c**2)
