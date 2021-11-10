5451e7182461e210f8cad3bd936d260a2616e3b2
Jordan Hay <jordanhay0112@gmail.com>
2021-11-11 10:27:13 +1300

    Bandage broken other module

--- jmath/units/other.py
+++ jmath/units/other.py
@@ -6,7 +6,7 @@
 
 from .units import Unit
 from .conversion import define_conversion, define_alias
-from .si import joule, kelvin, metre, second, kilogram, c, e
+from .si import si, c, e
 from math import pi
 
 # - Main
@@ -18,18 +18,18 @@ define_conversion(radian, degree, 180/pi)
 
 # Temperature
 centigrade = Unit("°C")
-define_conversion(centigrade, kelvin, lambda x: x + 273.15)
-define_conversion(kelvin, centigrade, lambda x: x - 273.15)
+define_conversion(centigrade, si.kelvin, lambda x: x + 273.15)
+define_conversion(si.kelvin, centigrade, lambda x: x - 273.15)
 
 # Physics
 # -- Light Speed
 lightspeed = Unit("c")
-define_conversion(lightspeed, metre/second, c)
+define_conversion(lightspeed, si.metre/si.second, c)
 # -- Electron Volts
 electron_volt = Unit("eV")
-define_conversion(electron_volt, joule, e)
+define_conversion(electron_volt, si.joule, e)
 # -- Electron Volt Mass Energy
 electron_volt_mass = electron_volt/(lightspeed**2)
 define_conversion(electron_volt_mass, electron_volt, 1)
-define_conversion(electron_volt_mass, kilogram, e/(c**2))
-define_conversion(electron_volt_mass, joule, e)
+define_conversion(electron_volt_mass, si.kilogram, e/(c**2))
+define_conversion(electron_volt_mass, si.joule, e)
