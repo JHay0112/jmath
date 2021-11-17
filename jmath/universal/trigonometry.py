22f1ed36e82a3e3472bec73bee80b2339c708f52
Jordan Hay <jordanhay0112@gmail.com>
2021-11-18 12:51:25 +1300

    Expand trigonometric functions

--- jmath/universal/trigonometry.py
+++ jmath/universal/trigonometry.py
@@ -22,6 +22,89 @@ def sin(input: other.radian) -> Supported:
         ----------
 
         input
-            Value to compute sine of
+            Value (in radians) to compute the sine of.
     '''
     return generic_function(math.sin, input)
+
+@annotate
+def asin(input: Supported) -> other.radian:
+    '''
+        Computes the inverse sine of the input.
+
+        Parameters
+        ----------
+
+        input
+            Value to compute the inverse sine of.
+
+        Returns
+        -------
+
+        Unit
+            Radians
+    '''
+    return generic_function(math.asin, input)
+
+@annotate
+def cos(input: other.radian) -> Supported:
+    '''
+        Computes the cosine of the input.
+
+        Parameters
+        ----------
+
+        input
+            Value (in radians) to compute the cosine of
+    '''
+    return generic_function(math.cos, input)
+
+@annotate
+def acos(input: Supported) -> other.radian:
+    '''
+        Computes the inverse cosine of the input.
+
+        Parameters
+        ----------
+
+        input
+            Value to compute the inverse cosine of.
+
+        Returns
+        -------
+
+        Unit
+            Radians
+    '''
+    return generic_function(math.acos, input)
+
+@annotate
+def tan(input: other.radian) -> Supported:
+    '''
+        Computes the tangent of the input.
+
+        Parameters
+        ----------
+
+        input
+            Value (in radians) to compute the tangent of
+    '''
+    return generic_function(math.tan, input)
+
+@annotate
+def atan(input: Supported) -> other.radian:
+    '''
+        Computes the inverse tangent of the input.
+
+        Parameters
+        ----------
+
+        input
+            Value to compute the inverse tangent of.
+
+        Returns
+        -------
+
+        Unit
+            Radians
+    '''
+    return generic_function(math.atan, input)
