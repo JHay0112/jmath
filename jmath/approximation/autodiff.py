a58f194f5e8a2d94d83bf22f718db1a3c56783cb
Jordan Hay <jordanhay0112@gmail.com>
2022-02-22 17:50:28 +1300

    Refined approach, still needs tweaked

--- jmath/approximation/autodiff.py
+++ jmath/approximation/autodiff.py
@@ -6,50 +6,102 @@
 
 import operator as op
 from ..uncertainties import Uncertainty
-from typing import Union, Callable
+from typing import Union, Callable, Tuple
 
-# - Globals
+# - Typing
 
-'''
-    A map of common functions and their partial derivates.
-    Partial derivatives are with respect to their positional relative parameter.
-'''
-diff_map = {
-    op.add: (1, 1),
-    op.sub: (1, -1),
-    op.mul: (
-        lambda x, y: y, 
-        lambda x, y: x
-    ),
-    op.truediv: (
-        lambda x, y: 1/y,
-        lambda x, y: -x/(y**2)
-    )
-}
+Supported = Union[int, float, Uncertainty, 'Function', 'Variable']
 
 # - Classes
 
 class Variable:
     '''
         Allows a function to be applied to a variable input.
-
-        Parameters
-        ----------
-
-        func
-            The function that owns (outputs to) the variable.
     '''
-    def __init__(self, func: 'Function' = None):
+    def __init__(self):
 
-        self.output_of = func
+        self.output_of = None
         self.input_of = set()
         self.value = None
 
-    def differentiate(self):
-        '''Differentiate the Variable'''
-        return 1
+    def __add__(self, other: Supported) -> 'Function':
 
-class Function:
+        if other == 0:
+            # Special case
+            return self
+        elif isinstance(other, (int, float, Uncertainty)):
+            # Numeric case
+            return Function(lambda x: x + other, lambda x: 1)
+        else:
+            # Variable case
+            return Function(op.add, (lambda x, y: 1, lambda x, y: 1))
+
+    def __radd__(self, other: Supported) -> 'Function':
+
+        return self + other
+
+    def __sub__(self, other: Supported) -> 'Function':
+
+        if other == 0:
+            # Special case
+            return self
+        elif isinstance(other, (int, float, Uncertainty)):
+            # Numeric case
+            return Function(lambda x: x - other, lambda x: 1)
+        else:
+            # Variable case
+            return Function(op.sub, (lambda x, y: 1, lambda x, y: -1))
+
+    def __rsub__(self, other: Supported) -> 'Function':
+
+        if isinstance(other, (int, float, Uncertainty)):
+            # Numeric case
+            return Function(lambda x: other - x, lambda x: -1)
+        else:
+            # Variable case
+            return Function(op.sub, (lambda x, y: 1, lambda x, y: -1))
+
+    def __mul__(self, other: Supported) -> 'Function':
+
+        if other == 1:
+            # Special case
+            return self
+        elif isinstance(other, (int, float, Uncertainty)):
+            # Numeric case
+            return Function(lambda x: other * x, lambda x, y: other)
+        else:
+            # Variable case
+            return Function(op.mul, (lambda x, y: y, lambda x, y: x))
+
+    def __rmul__(self, other: Supported) -> 'Function':
+
+        return self * other
+
+    def __truediv__(self, other: Supported) -> 'Function':
+
+        if other == 1:
+            # Special case
+            return other
+        elif other == 0:
+            # Error case
+            raise ZeroDivisionError
+        elif isinstance(other, (int, float, Uncertainty)):
+            # Numeric case
+            return Function(lambda x: x/other, lambda x: 1/other)
+        else:
+            # Variable case
+            return Function(op.truediv, (lambda x, y: 1/y, lambda x, y: -x/(y**2)))
+
+    def __rtruediv__(self, other: Supported) -> 'Function':
+
+        if isinstance(other, (int, float, Uncertainty)):
+            # Numeric case
+            return Function(lambda x: other/x, lambda x: -other/(x**2))
+        else:
+            # Variable case
+            return Function(op.rtruediv, (lambda x, y: 1/y, lambda x, y: -x/(y**2)))
+
+class Function(Variable):
     '''
         A Differentiable Function.
 
@@ -58,12 +110,21 @@ class Function:
 
         func
             The callable function to be represented.
+        diff
+            A tuple of functions produced upon differentiation with respect to the function variables.
     '''
-    def __init__(self, func: Callable):
+    def __init__(self, func: Callable, diff: Tuple[Callable]):
 
         self.vars = []
         self.func = func
-        self.output = Variable(self)
+        self.diff = diff
+        self.output = Variable()
+        self.output.output_of = self
+
+        # Check if diff is not a tuple
+        if not isinstance(diff, tuple):
+            # If not then we shall make it one
+            self.diff = (diff,)
 
     def __call__(self, *inputs: Union[int, float, Uncertainty, Variable, 'Function']) -> Union[int, float, Uncertainty, 'Function']:
         '''Evaluate the function.'''
@@ -92,8 +153,12 @@ class Function:
                     input.output.input_of.add(self)
             # Let's return the function for use
             return self
+        else:
+            # Real inputs
+            # Evaluate the function
+            return self.func(*inputs)
 
-    def diff_wrt(self, var: Variable) -> 'Function':
+    def differentiate(self, var: Variable) -> 'Function':
         '''
             Produces the partial differential of the function with respect to the specified variable.
 
@@ -103,12 +168,16 @@ class Function:
             var
                 The variable to differentiate with repsect to.
         '''
-        # Get index of variable for THIS function
-        index = self.vars.index(var)
-        # Now use diff map to get the diff of this function
-        diff_func = diff_map(self.func)
-
-# - Definitions
-
-add = Function(op.add)
-mul = Function(op.mul)
+        # Derivative produced
+        func = 0
+        # Go down each 'branch'
+        for i, input_var in enumerate(self.vars):
+            branch = self.diff[i]
+            # Check if it is 'owned' by a function
+            if input_var.output_of is not None:
+                # Then derive that function by the same variable and add it
+                func += branch * input_var.output_of.differentiate(var)
+            else:
+                func = branch
+        
+        return func
